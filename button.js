66e7a6b80f0c80dab4a28c8dbae8cf75b4dbb83f
soroz30 <soroz30@yahoo.pl>
2017-10-11 13:55:15 +0200

    Zadanie 11.5

--- button.js
+++ button.js
@@ -3,15 +3,15 @@ function Button(text) {
 }
 
 Button.prototype = {
-	create: function() {
-		var self = this;
-		this.$element = $('<button>');
-		this.$element.text(this.text);
-		this.$element.click(function() {
-			alert(self.text)
-		});
-		this.$element.appendTo($('body'));
-	}
+    create: function() {
+        var self = this;
+        this.$element = $('<button>');
+        this.$element.text(this.text);
+        this.$element.click(function() {
+            alert(self.text)
+        });
+        this.$element.appendTo($('body'));
+    }
 }
 
 var btn1 = new Button('Hello!');
